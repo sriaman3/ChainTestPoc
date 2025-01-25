c6fc4cca3c6a813829cdc52777caefe8d30bff2f
Aman <sriaman3@gmail.com>
2025-01-25 20:06:11 +0530

    ChainTest POC

---
+++ src/test/java/org/qa/opencart/tests/AccountTest.java
@@ -0,0 +1,20 @@
+package org.qa.opencart.tests;
+
+import org.qa.opencart.base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class AccountTest extends BaseTest {
+	
+	@BeforeClass
+	public void accLogin() {
+		accPage = lp.doLogin("aman@opencart.com", "12345");
+	}
+	
+	@Test
+	public void verifyAccountPageTitle() {
+		Assert.assertEquals(accPage.getAccountPageTitle(), "My Account");
+	}
+
+}
